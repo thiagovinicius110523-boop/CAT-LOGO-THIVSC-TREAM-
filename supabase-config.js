6921b4756f888b52005499ed4aae035eb546c740
thiagovinicius110523-boop <thiagovinicius110523@gmail.com>
2026-02-20 15:41:03 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -10,7 +10,7 @@ window.SUPABASE_URL = window.SUPABASE_URL || "https://jayfvtrmgpezoixijhad.supab
 window.SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpheWZ2dHJtZ3Blem9peGlqaGFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MjUzMDQsImV4cCI6MjA4NzAwMTMwNH0.UzN6pLWpBPw0__mRoLpB0obi4qemmZ0rmUKEVhVM1Qc";
 
 /**
- * APP_BASE_URL ()
+ * APP_BASE_URL (https://thiagovinicius110523-boop.github.io/CAT-LOGO-THIVSC-STREAM)
  * Use quando estiver em GitHub Pages com subpasta, ex:
  * https://usuario.github.io/REPO/
  */
