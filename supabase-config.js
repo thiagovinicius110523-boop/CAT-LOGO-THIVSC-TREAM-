6246bc9be4c732dfcc9003dbd3a1f235841db946
thiagovinicius110523-boop <thiagovinicius110523@gmail.com>
2026-02-21 20:17:00 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -6,7 +6,7 @@ const SUPABASE_URL = "https://jayfvtrmgpezoixijhad.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpheWZ2dHJtZ3Blem9peGlqaGFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MjUzMDQsImV4cCI6MjA4NzAwMTMwNH0.UzN6pLWpBPw0__mRoLpB0obi4qemmZ0rmUKEVhVM1Qc";
 
 // IMPORTANTE: sem barra no final
-const APP_BASE_URL = "https://thiagovinicius110523-boop.github.io/CAT-THIVSC-STREAM-V2";
+const APP_BASE_URL = "https://thiagovinicius110523-boop.github.io/CAT-THIVSC-STREAM-V2/";
 
 // Exporta para uso global
 window.SUPABASE_URL = SUPABASE_URL;
