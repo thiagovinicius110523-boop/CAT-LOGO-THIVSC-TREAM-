dd8213b4302de6b4839e564b97e5d2d6624e81b8
thiagovinicius110523-boop <thiagovinicius110523@gmail.com>
2026-02-21 22:21:55 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-window.SUPABASE_URL = "https://jayfvtrmgpezoixijhad.supabase.co";
-
-window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpheWZ2dHJtZ3Blem9peGlqaGFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE0MjUzMDQsImV4cCI6MjA4NzAwMTMwNH0.UzN6pLWpBPw0__mRoLpB0obi4qemmZ0rmUKEVhVM1Qc";
-
-window.APP_BASE_URL = "https://thiagovinicius110523-boop.github.io/CAT-THIVSC-STREAM-V2/";
